da77d43af615f420d16c607b1a3b43a8fcc5adcb
Ross Simpson <ross.simpson@envato.com>
2016-02-12 12:20:19 +1100

    Install jdk if missing
    To be moved to the AMI.

--- scripts/buildkite_release.sh
+++ scripts/buildkite_release.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# hackity hack hack hack!  Move this to the AMI
+javac -version 2>/dev/null || sudo apt-get install -y openjdk-7-jdk
+
+./mvnw clean package
+
 echo "Assuming IAM role to access discovery-configuration S3 bucket"
 eval $(aws sts assume-role \
   --role-arn arn:aws:iam::932328744288:role/pushToS3Role \
@@ -12,8 +17,6 @@ PLUGIN_LOCATION="s3://discovery-configuration/config/elasticsearch/plugins/"
 
 # Move existing gems into the old/ dir
 aws s3 mv "${PLUGIN_LOCATION}" "${PLUGIN_LOCATION}old/" --recursive --exclude "old"
-
-./mvnw clean package
 
 # Upload the newly-built artifact
 aws s3 cp target/releases/elasticsearch-riemann-*.zip "${PLUGIN_LOCATION}"
